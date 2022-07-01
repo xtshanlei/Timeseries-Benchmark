b980fc4be6f697b6f7fe771194a4945552b10a24
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-07-01 10:53:29 +0100

    test2

--- timeseries_benchmark.py
+++ timeseries_benchmark.py
@@ -17,3 +17,5 @@ uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
     dataframe = pd.read_csv(uploaded_file)
     st.write(dataframe)
+
+#######Choose darts models#######
