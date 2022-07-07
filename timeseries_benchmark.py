d6b5c0c2c6b0c12a4d566a7c076395549426b58d
xtshanlei <49090451+xtshanlei@users.noreply.github.com>
2022-07-07 14:47:01 +0100

    Update timeseries_benchmark.py

--- timeseries_benchmark.py
+++ timeseries_benchmark.py
@@ -18,34 +18,36 @@ if uploaded_file is not None:
     st.write(dataframe)
 
 #######Choose the time series column#######
-st.header('2. Which column to forecast?')
-timeseries_to_forecast = st.selectbox(
-                                  "Please choose the column that you want to forecast",
-                                  dataframe.columns
-                                    )
-date_column = st.selectbox(
-                                  "Please choose the column of the date",
-                                  dataframe.columns
-                                    )
+if uploaded_file:
+    st.header('2. Which column to forecast?')
+    timeseries_to_forecast = st.selectbox(
+                                      "Please choose the column that you want to forecast",
+                                      dataframe.columns
+                                        )
+    date_column_name = st.selectbox(
+                                      "Please choose the column of the date",
+                                      dataframe.columns
+                                        )
 
-date_format = st.text_input("Please indicate the date format below (%Y: year; %m: month; %d: day; ")
-#######Choose darts models#######
-st.header('3. Model selection')
-selected_model_list = st.multiselect(
-                                    "Please choose model that you want to use",
-                                    ['Arima','FFT','FacebookProphet','TCN','LSTM','NBEATS']
-                                    )
-paras ={
-    "dataframe":dataframe,
-    "date_column":date_column,
-    "date_format":date_format,
-    "split_point":'2017-03-27',
-    "y_column":timeseries_to_forecast,
-    "num_lags":1,
-    "input_length":30,
-    "kernel_size":12,
-    "model_list": ['Arima','FFT','FacebookProphet','TCN','LSTM','NBEATS'],
-    "scaled": True,
-    }
-london_model = BenchModel(**paras)
-pred_london_df,metric_london_df = london_model.bench_compare()
+    date_format = st.text_input("Please indicate the date format below (%Y: year; %m: month; %d: day; ")
+    #######Choose darts models#######
+    st.header('3. Model selection')
+    selected_model_list = st.multiselect(
+                                        "Please choose model that you want to use",
+                                        ['Arima','FFT','FacebookProphet','TCN','LSTM','NBEATS']
+                                        )
+    if timeseries_to_forecast and date_column_name and date_format and selected_model_list:
+        paras ={
+            "dataframe":dataframe,
+            "date_column":date_column_name,
+            "date_format":date_format,
+            "split_point":'2017-03-27',
+            "y_column":timeseries_to_forecast,
+            "num_lags":1,
+            "input_length":30,
+            "kernel_size":12,
+            "model_list": selected_model_list,
+            "scaled": True,
+            }
+        london_model = BenchModel(**paras)
+        pred_london_df,metric_london_df = london_model.bench_compare()
